c18b2ea5c769fdef0576f64dfc331fc47579728f
alina <130225239+alina15andreeva@users.noreply.github.com>
2023-05-29 01:36:39 +0300

    hw9

---
+++ task40.py
@@ -0,0 +1,4 @@
+# Задача 40: Работать с файлом california_housing_train.csv, который находится в папке
+# sample_data. Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)
+
+df[(df['population'] > 0) & (df['population'] < 500)]['median_house_value'].mean()
